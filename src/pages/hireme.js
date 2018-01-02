0baebc69ccfed029814ea0008ef606e1ef79025e
Lars Graubner <mail@larsgraubner.de>
2018-01-02 20:49:34 +0100

    adjust hireme

--- src/pages/hireme.js
+++ src/pages/hireme.js
@@ -17,17 +17,32 @@ const SubTitle = styled.h2`
   color: rgba(0, 0, 0, 0.8);
 `
 
+const Heading = styled.h3`
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: rgba(0, 0, 0, 0.8)
+  margin: 0 0 2rem;
+`
+
 const Index = () => (
   <div>
     <Helmet>
       <title>Hire me - Lars Graubner</title>
     </Helmet>
-    <Title>Work with me</Title>
-    <SubTitle>The requested page could not be found</SubTitle>
+    <Title>Hi, I{"'"}m Lars</Title>
+    <SubTitle>
+      A passionate front-end developer based in Lübeck, Germany.
+    </SubTitle>
     <LimitedText>
-      Please contact the owner of the site that linked you to the original URL
-      and let them know their link is broken.
+      I{"'"}m focusing on JavaScript and React single page application
+      development.
     </LimitedText>
+    <Heading>I{"'"}m offering</Heading>
+    <ul>
+      <li>React Consulting</li>
+      <li>Single Page Applications</li>
+    </ul>
+    <Heading>What customers say</Heading>
   </div>
 )
 
